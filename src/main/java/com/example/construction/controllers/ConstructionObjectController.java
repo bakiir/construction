b32708df08a1adcb6dc443637d5f9e47d0f50a79
bakiir <bkiranarbek@gmail.com>
2026-01-18 20:32:29 +0600

    Finished: crud for objects

--- src/main/java/com/example/construction/controllers/ConstructionObjectController.java
+++ src/main/java/com/example/construction/controllers/ConstructionObjectController.java
@@ -5,10 +5,9 @@ import com.example.construction.dto.ConstructionObjectDto;
 import com.example.construction.mapper.ConstructionObjectMapper;
 import com.example.construction.service.ConstructionObjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,4 +20,27 @@ public class ConstructionObjectController {
     public ConstructionObjectDto create(@RequestBody ConstructionObjectCreateDto dto) {
         return service.create(dto);
     }
+
+    @GetMapping("/{id}")
+    public ConstructionObjectDto getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @GetMapping("/project/{projectId}")
+    public List<ConstructionObjectDto> getByProject(@PathVariable Long projectId) {
+        return service.getByProject(projectId);
+    }
+
+    @PutMapping("/{id}")
+    public ConstructionObjectDto update(
+            @PathVariable Long id,
+            @RequestBody ConstructionObjectCreateDto dto) {
+        return service.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+
 }
